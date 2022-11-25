96c6d340ca52983abaff6a21cd21672fcb90c9ba
Paul Laurent <plga.laurent@orange.fr>
2022-11-25 12:19:03 +0100

    Fichier 2

---
+++ paul.py
@@ -0,0 +1,25 @@
+{
+'Chenipan': {'Numéro': 10, 'Type': ('Insecte',), 
+'Stats': {'PV': 45, 'Att': 30, 'Def': 35, 'SpA': 20, 'SpD': 20, 'Vit': 45}, 
+'Evolue': 'Niveau 7'},
+
+'Chrysacier': {'Numéro': 11, 'Type': ('Insecte',), 
+'Stats': {'PV': 50, 'Att': 20, 'Def': 55, 'SpA': 25, 'SpD': 25, 'Vit': 30}, 
+'Evolue': 'Niveau 10'},
+
+'Papilusion': {'Numéro': 12, 'Type': ('Insecte','Vol'), 
+'Stats': {'PV': 60, 'Att': 45, 'Def': 50, 'SpA': 90, 'SpD': 80, 'Vit': 70}, 
+'Evolue': None},
+
+'Aspicot': {'Numéro': 13, 'Type': ('Insecte','Poison'), 
+'Stats': {'PV': 40, 'Att': 35, 'Def': 30, 'SpA': 20, 'SpD': 20, 'Vit': 50}, 
+'Evolue': 'Niveau 7'},
+
+'Conconfort': {'Numéro': 14, 'Type': ('Insecte','Poison'), 
+'Stats': {'PV': 45, 'Att': 25, 'Def': 50, 'SpA': 25, 'SpD': 25, 'Vit': 35}, 
+'Evolue': 'Niveau 10'},
+
+'Dardargnan': {'Numéro': 15, 'Type': ('Insecte','Poison'), 
+'Stats': {'PV': 65, 'Att': 90, 'Def': 40, 'SpA': 45, 'SpD': 80, 'Vit': 75}, 
+'Evolue': None},
+}
