37a35d0dbb477d128d3c936799b349f7c8adf38a
Paul Laurent <plga.laurent@orange.fr>
2022-11-25 11:48:04 +0100

    AJout du fichier

---
+++ base.py
@@ -0,0 +1,40 @@
+
+d = {
+'Bulbizarre': {'Numéro': 1, 'Type': ('Plante','Poison'), 
+'Stats': {'PV': 45, 'Att': 49, 'Def': 49, 'SpA': 65, 'SpD': 65, 'Vit': 45}, 
+'Evolue':'Niveau 16'},
+
+'Herbizarre': {'Numéro': 2, 'Type': ('Plante','Poison'), 
+'Stats': {'PV': 60, 'Att': 62, 'Def': 63, 'SpA': 80, 'SpD': 80, 'Vit': 60}, 
+'Evolue': 'Niveau 32'},
+
+'Florizarre': {'Numéro': 3, 'Type': ('Plante','Poison'), 
+'Stats': {'PV': 80, 'Att': 82, 'Def': 83, 'SpA': 100, 'SpD': 100, 'Vit': 80}, 
+'Evolue': None},
+
+'Salamèche': {'Numéro': 4, 'Type': ('Feu',), 
+'Stats': {'PV': 39, 'Att': 52, 'Def': 43, 'SpA': 60, 'SpD': 50, 'Vit': 65}, 
+'Evolue': 'Niveau 16'},
+
+'Reptincel': {'Numéro': 5, 'Type': ('Feu',), 
+'Stats': {'PV': 58, 'Att': 64, 'Def': 58, 'SpA': 80, 'SpD': 65, 'Vit': 80}, 
+'Evolue': 'Niveau 36'},
+
+'Dracaufeu': {'Numéro': 6, 'Type': ('Feu','Vol'), 
+'Stats': {'PV': 78, 'Att': 84, 'Def': 78, 'SpA': 109, 'SpD': 85, 'Vit': 100}, 
+'Evolue': None},
+
+'Carapuce': {'Numéro': 7, 'Type': ('Eau',), 
+'Stats': {'PV': 44, 'Att': 48, 'Def': 65, 'SpA': 50, 'SpD': 64, 'Vit': 43}, 
+'Evolue': 'Niveau 16'},
+
+'Carabaffe': {'Numéro': 8, 'Type': ('Eau',), 
+'Stats': {'PV': 59, 'Att': 63, 'Def': 80, 'SpA': 65, 'SpD': 80, 'Vit': 58}, 
+'Evolue': 'Niveau 36'},
+
+'Tortank': {'Numéro': 9, 'Type': ('Eau',), 
+'Stats': {'PV': 79, 'Att': 83, 'Def': 100, 'SpA': 85, 'SpD': 105, 'Vit': 78}, 
+'Evolue': None},
+}
+
+print(d['Bulbizarre']['Evolue au niveau'])
